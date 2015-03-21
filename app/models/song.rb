ba86605b6585349791ccf2f21a15bd75b149227f
Eric Stiens <estiens@gmail.com>
2015-03-21 02:29:25 -0500

    add rails admin

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ActiveRecord::Base
+
+  rails_admin do
+    edit do
+      field :title
+      field :lyrics, :ck_editor
+      field :soundcloud_url
+      field :original_song_url
+    end
+  end
+
 end
