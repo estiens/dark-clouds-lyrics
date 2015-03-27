869c10c88a7bf5beea3d9821b139f8bddfec893f
Eric Stiens <estiens@gmail.com>
2015-03-27 14:42:50 -0500

    try to get ckeditor working on heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")
 Rails.application.config.assets.precompile += %w( jquerymobile.blue.css jquerymobile.nativedroid.css jquerymobile.nativedroid.dark.css )
+Rails.application.config.assets.precompile += %w(rails_admin/rails_admin.js rails_admin/rails_admin.css rails_admin/jquery.colorpicker.js rails_admin/jquery.colorpicker.css ckeditor/*)
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
